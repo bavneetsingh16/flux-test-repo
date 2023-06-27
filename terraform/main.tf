50ad9fbf3ce4b65ea677f1d9b8aa204647265c3a
Bavneet Singh <bavneetsingh@microsoft.com>
2023-06-27 16:50:57 -0700

    add terraform test

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 0.12.26"
+}
+
+variable "secret_subject" {
+    type = string
+    sensitive = true
+    description = "Secret subject"
+}
+
+variable "subject" {
+   type = list
+   description = "Subject to hello"
+}
+
+output "hello_world" {
+  sensitive = true
+  value = "Hello, ${var.subject}! (secret: ${var.secret_subject})"
+}
